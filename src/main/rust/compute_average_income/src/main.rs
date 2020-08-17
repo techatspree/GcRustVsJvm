defc1b631397ed0c6074668ef1df6a54d7c2e34f
Torsten Fink <torsten.fink@akquinet.de>
2020-08-17 17:26:25 +0200

    first running version to create Employees

--- src/main/rust/compute_average_income/src/main.rs
+++ src/main/rust/compute_average_income/src/main.rs
@@ -30,20 +30,30 @@ fn create_random_employee(char_pool: &Vec<char>) -> Employee {
         address: Address
         {
             street: createRandomStringOf80Chars(char_pool),
-            postal_code : createRandomStringOf80Chars(char_pool),
+            postal_code: createRandomStringOf80Chars(char_pool),
             city: createRandomStringOf80Chars(char_pool),
-            country : createRandomStringOf80Chars(char_pool)
+            country: createRandomStringOf80Chars(char_pool),
         },
-        salary: 1000
+        salary: 1000,
     };
+}
+
+fn lookup_all_employees(number_of_all_employees: i64, char_pool: &Vec<char>) -> Vec<Employee> {
+    return
+        (0..number_of_all_employees)
+            .map(|n| { return create_random_employee(char_pool); })
+            .collect();
 }
 
 fn main() {
     let v = (1u64..11).collect::<Vec<_>>();
-    println!("v: {:#?}", v);
     let char_pool = ('a'..'z').collect::<Vec<_>>();
+    let number_of_all_employees: i64 = 1000000;
+
     println!("random string: {:#?}", createRandomStringOf80Chars(&char_pool));
     let employee = create_random_employee(&char_pool);
     dbg!(employee);
     //println!("{:#?}", employee)
+    let empl = lookup_all_employees(5, &char_pool);
+    dbg!(empl);
 }
