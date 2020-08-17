f6c908aef0096554cc66ecba7749159d45965a93
Torsten Fink <torsten.fink@akquinet.de>
2020-08-17 17:27:58 +0200

    clean ups

--- src/main/rust/compute_average_income/src/main.rs
+++ src/main/rust/compute_average_income/src/main.rs
@@ -17,7 +17,7 @@ struct Employee {
 }
 
 
-fn createRandomStringOf80Chars(char_pool: &Vec<char>) -> String {
+fn create_random_string_of_80_chars(char_pool: &Vec<char>) -> String {
     return (0..80)
         .map(|_| { char_pool[rand::thread_rng().gen_range(0, char_pool.len())] })
         .into_iter().collect();
@@ -25,14 +25,14 @@ fn createRandomStringOf80Chars(char_pool: &Vec<char>) -> String {
 
 fn create_random_employee(char_pool: &Vec<char>) -> Employee {
     return Employee {
-        first_name: createRandomStringOf80Chars(char_pool),
-        last_name: createRandomStringOf80Chars(char_pool),
+        first_name: create_random_string_of_80_chars(char_pool),
+        last_name: create_random_string_of_80_chars(char_pool),
         address: Address
         {
-            street: createRandomStringOf80Chars(char_pool),
-            postal_code: createRandomStringOf80Chars(char_pool),
-            city: createRandomStringOf80Chars(char_pool),
-            country: createRandomStringOf80Chars(char_pool),
+            street: create_random_string_of_80_chars(char_pool),
+            postal_code: create_random_string_of_80_chars(char_pool),
+            city: create_random_string_of_80_chars(char_pool),
+            country: create_random_string_of_80_chars(char_pool),
         },
         salary: 1000,
     };
@@ -46,14 +46,12 @@ fn lookup_all_employees(number_of_all_employees: i64, char_pool: &Vec<char>) -> 
 }
 
 fn main() {
-    let v = (1u64..11).collect::<Vec<_>>();
     let char_pool = ('a'..'z').collect::<Vec<_>>();
-    let number_of_all_employees: i64 = 1000000;
 
-    println!("random string: {:#?}", createRandomStringOf80Chars(&char_pool));
+    println!("random string: {:#?}", create_random_string_of_80_chars(&char_pool));
     let employee = create_random_employee(&char_pool);
     dbg!(employee);
     //println!("{:#?}", employee)
-    let empl = lookup_all_employees(5, &char_pool);
-    dbg!(empl);
+    let employees = lookup_all_employees(5, &char_pool);
+    dbg!(employees);
 }
