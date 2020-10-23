01bc6b98cd28acaea0a47c5579286d5f87dc1bd5
Torsten Fink <torsten.fink@akquinet.de>
2020-10-23 21:30:20 +0200

    added first test

--- src/main/Haskell/compute-average-income/test/Spec.hs
+++ src/main/Haskell/compute-average-income/test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import ComputeAverageIncome
+
+prop_createRandomStringOf80Chars :: Property
+
+prop_createRandomStringOf80Chars = monadicIO $ do
+  string <- run createRandomStringOf80Chars
+  assert ((length string) == 80)
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_createRandomStringOf80Chars
+  putStrLn "Tests done"
