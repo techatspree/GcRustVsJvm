f97e67b8121b3ab6c131a63d0cb9ab44cf83028d
Torsten Fink <torsten.fink@akquinet.de>
2020-10-25 11:33:49 +0100

    added random address

--- src/main/Haskell/compute-average-income/src/ComputeAverageIncome.hs
+++ src/main/Haskell/compute-average-income/src/ComputeAverageIncome.hs
@@ -1,6 +1,7 @@
 module ComputeAverageIncome
     ( computeAverageIncomeOfAllEmployees
     , createRandomStringOf80Chars
+    , createRandomAddress
     ) where
 
 import System.Random
@@ -10,14 +11,14 @@ data Address = Address
   , postalCode:: String
   , city:: String
   , country:: String
-  }
+  } deriving (Eq)
 
 data Employee = Employee
   { firstName:: String
   , lastName:: String
   , address:: Address
   , salary:: Int
-  }
+  } deriving (Eq)
 
 -- todo: I should use the same set of chars as the other implementations
 -- todo: I could use a real String instead of a list, but I do not need to
@@ -25,6 +26,19 @@ createRandomStringOf80Chars :: IO String
 createRandomStringOf80Chars = do
   random <- newStdGen
   return $ take 80 $ randomRs ('a','z') random
+
+createRandomAddress :: IO Address
+createRandomAddress = do
+  streetV <- createRandomStringOf80Chars
+  postalCodeV <- createRandomStringOf80Chars
+  cityV <- createRandomStringOf80Chars
+  countryV <- createRandomStringOf80Chars
+  return Address
+    { street = streetV
+    , postalCode = postalCodeV
+    , city = cityV
+    , country = countryV
+    }
 
 computeAverageIncomeOfAllEmployees :: IO ()
 computeAverageIncomeOfAllEmployees = putStrLn "someFunc"
--- src/main/Haskell/compute-average-income/test/Spec.hs
+++ src/main/Haskell/compute-average-income/test/Spec.hs
@@ -3,13 +3,19 @@ import Test.QuickCheck.Monadic
 import ComputeAverageIncome
 
 prop_createRandomStringOf80Chars :: Property
-
 prop_createRandomStringOf80Chars = monadicIO $ do
   string <- run createRandomStringOf80Chars
   assert ((length string) == 80)
 
+prop_createRandomAddress :: Property
+prop_createRandomAddress = monadicIO $ do
+  address1 <- run createRandomAddress
+  address2 <- run createRandomAddress
+  assert (address1 /= address2)
 
 main :: IO ()
 main = do
+  putStrLn "Starting tests"
   quickCheck prop_createRandomStringOf80Chars
+  quickCheck prop_createRandomAddress
   putStrLn "Tests done"
