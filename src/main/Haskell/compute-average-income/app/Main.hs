e04a148f0310c34b9a4ae86bedf4e405059a8139
Torsten Fink <torsten.fink@akquinet.de>
2020-10-30 19:19:24 +0100

    adding strict Haskell implementation

--- src/main/Haskell/compute-average-income/app/Main.hs
+++ src/main/Haskell/compute-average-income/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Criterion.Main
 import ComputeAverageIncome
+import ComputeAverageIncomeStrict
 
 main :: IO ()
 main = defaultMain [
@@ -10,6 +11,12 @@ main = defaultMain [
           , bench "10^4"  $ nfIO (computeAverageIncomeOfAllEmployees 10000)
           , bench "10^5"  $ nfIO (computeAverageIncomeOfAllEmployees 100000)
           , bench "10^6"  $ nfIO (computeAverageIncomeOfAllEmployees 1000000)
+          ],
+        bgroup "compute average income strict"
+          [ bench "10^3"  $ nfIO (computeAverageIncomeOfAllEmployeesStrict 1000)
+          , bench "10^4"  $ nfIO (computeAverageIncomeOfAllEmployeesStrict 10000)
+          , bench "10^5"  $ nfIO (computeAverageIncomeOfAllEmployeesStrict 100000)
+          , bench "10^6"  $ nfIO (computeAverageIncomeOfAllEmployeesStrict 1000000)
           ]
       ]
 
---
+++ src/main/Haskell/compute-average-income/src/ComputeAverageIncomeStrict.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE  Strict #-}
+module ComputeAverageIncomeStrict
+    ( computeAverageIncomeOfAllEmployeesStrict
+    ) where
+
+import System.Random
+import Control.Monad
+
+data Address = Address
+  { street:: String
+  , postalCode:: String
+  , city:: String
+  , country:: String
+  } deriving (Eq)
+
+data Employee = Employee
+  { firstName:: String
+  , lastName:: String
+  , address:: Address
+  , salary:: Int
+  } deriving (Eq)
+
+chars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
+nrOfChars = length chars
+
+createRandomStringOf80Chars :: IO String
+createRandomStringOf80Chars = do
+  random <- newStdGen
+  return $
+    take 80
+      $ map (\i -> chars !! i)
+      $ randomRs ( 0,nrOfChars -1) random
+
+createRandomAddress :: IO Address
+createRandomAddress = do
+  streetV <- createRandomStringOf80Chars
+  postalCodeV <- createRandomStringOf80Chars
+  cityV <- createRandomStringOf80Chars
+  countryV <- createRandomStringOf80Chars
+  return Address
+    { street = streetV
+    , postalCode = postalCodeV
+    , city = cityV
+    , country = countryV
+    }
+
+createRandomEmployee :: IO Employee
+createRandomEmployee = do
+  firstNameV <- createRandomStringOf80Chars
+  lastNameV <- createRandomStringOf80Chars
+  addressV <- createRandomAddress
+  let salaryV = 1000
+  return Employee
+    { firstName = firstNameV
+    , lastName = lastNameV
+    , address = addressV
+    , salary = salaryV
+}
+
+lookupAllEmployees :: Int -> IO [Employee]
+lookupAllEmployees numberOfAllEmployees =
+  replicateM numberOfAllEmployees createRandomEmployee
+
+computeAverageIncome :: [Employee] -> Float
+computeAverageIncome employees =
+  let (nrEmployees, sumOfAllSalaries) =
+        foldl (\ (counter, sum) employee -> (counter + 1, sum + (salary employee)))
+              (0,0) employees
+  in
+    (fromIntegral sumOfAllSalaries) / (fromIntegral nrEmployees)
+
+computeAverageIncomeOfAllEmployeesStrict :: Int -> IO (Float)
+computeAverageIncomeOfAllEmployeesStrict numberOfAllEmployees = do
+  employees <- lookupAllEmployees numberOfAllEmployees
+  --let average = computeAverageIncome employees
+  --putStrLn ("average: " ++ (show average))
+  return $ computeAverageIncome employees
