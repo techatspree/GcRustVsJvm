2f245662c7cdbb6e3e17a1cfbb16600da6390d8f
Paul Schyska <Paul.Schyska@akquinet.de>
2020-11-02 13:00:49 +0100

    Make RNG configurable and switch to a non-cryptographic rng. Sample from Uniform for an extra speedup

--- src/main/Rust/compute_average_income/src/main.rs
+++ src/main/Rust/compute_average_income/src/main.rs
@@ -1,5 +1,5 @@
-use rand::Rng;
-use std::time::{Instant};
+use rand::{distributions::Uniform, rngs::SmallRng, Rng, RngCore, SeedableRng};
+use std::time::Instant;
 
 #[derive(Debug)]
 struct Address {
@@ -16,56 +16,69 @@ struct Employee {
     salary: u64,
 }
 
-fn create_random_string_of_80_chars(char_pool: &Vec<char>) -> String {
-    return (0..80)
-        .map(|_| { char_pool[rand::thread_rng().gen_range(0, char_pool.len())] })
-        .into_iter().collect();
+struct RandyRandom {
+    rng: Box<dyn RngCore>,
+    pool: Vec<char>,
+    dist: Uniform<usize>,
 }
 
-fn create_random_employee(char_pool: &Vec<char>) -> Employee {
-    return Employee {
-        first_name: create_random_string_of_80_chars(char_pool),
-        last_name: create_random_string_of_80_chars(char_pool),
-        address: Address
-        {
-            street: create_random_string_of_80_chars(char_pool),
-            postal_code: create_random_string_of_80_chars(char_pool),
-            city: create_random_string_of_80_chars(char_pool),
-            country: create_random_string_of_80_chars(char_pool),
-        },
-        salary: 1000,
-    };
+impl RandyRandom {
+    fn new(rng: Box<dyn RngCore>) -> RandyRandom {
+        let pool: Vec<_> = ('a'..'z').collect();
+        let dist = Uniform::from(0..pool.len());
+        RandyRandom { rng, pool, dist }
+    }
+
+    fn create_random_employee(&mut self) -> Employee {
+        Employee {
+            first_name: self.create_random_string_of_80_chars(),
+            last_name: self.create_random_string_of_80_chars(),
+            address: Address {
+                street: self.create_random_string_of_80_chars(),
+                postal_code: self.create_random_string_of_80_chars(),
+                city: self.create_random_string_of_80_chars(),
+                country: self.create_random_string_of_80_chars(),
+            },
+            salary: 1000,
+        }
+    }
+
+    fn create_random_string_of_80_chars(&mut self) -> String {
+        (0..80)
+            .map(|_| self.pool[self.rng.sample(self.dist)])
+            .collect()
+    }
 }
 
-fn lookup_all_employees<'a>(number_of_all_employees: u64, char_pool: &'a Vec<char>)
-                            -> impl Iterator<Item=Employee> + 'a {
-    return
-        (0..number_of_all_employees)
-            .map(move |_| { return create_random_employee(char_pool); })
-            .into_iter();
+fn lookup_all_employees(
+    number_of_all_employees: u64,
+    r: &mut RandyRandom,
+) -> impl Iterator<Item = Employee> + '_ {
+    (0..number_of_all_employees).map(move |_| r.create_random_employee())
 }
 
-fn compute_average_income_of_all_employees(employees: impl Iterator<Item=Employee>)
-                                           -> f64 {
-    let (num_of_employees, sum_of_salaries) =
-        employees.fold((0u64, 0u64),
-                       |(counter, sum), employee| {
-                           return (counter + 1, sum + employee.salary);
-                       });
-    return (sum_of_salaries as f64) / (num_of_employees as f64);
+fn compute_average_income_of_all_employees(employees: impl Iterator<Item = Employee>) -> f64 {
+    let (num_of_employees, sum_of_salaries) = employees
+        .fold((0u64, 0u64), |(counter, sum), employee| {
+            (counter + 1, sum + employee.salary)
+        });
+    (sum_of_salaries as f64) / (num_of_employees as f64)
 }
 
 fn main() {
-    let char_pool = ('a'..'z').collect::<Vec<_>>();
-
     let nrs_of_employees = [1000u64, 10000, 100000, 1000000];
+    let mut r = RandyRandom::new(Box::new(SmallRng::from_entropy()));
     for nr_of_employees in &nrs_of_employees {
         let start_time = Instant::now();
-        let average = compute_average_income_of_all_employees(lookup_all_employees(
-            *nr_of_employees, &char_pool,
-        ));
+        let average =
+            compute_average_income_of_all_employees(lookup_all_employees(*nr_of_employees, &mut r));
         let end_time = Instant::now();
         let duration = end_time.duration_since(start_time);
-        println!("n={} Average = {} Duration = {}ms", nr_of_employees, average, duration.as_millis());
+        println!(
+            "n={} Average = {} Duration = {}ms",
+            nr_of_employees,
+            average,
+            duration.as_millis()
+        );
     }
 }
